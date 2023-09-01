3a43084e9034a0ee768cdda2a5963aa1d310d110
truongminh <nguyentruongminh7@gmail.com>
2023-09-01 18:57:46 +0700

    fix: upload xml files

---
+++ upload/index.mjs
@@ -0,0 +1,51 @@
+import { ExtractInspections } from "./xml_data.mjs";
+import { MongoClient } from 'mongodb';
+
+// CONFIG
+const url = 'mongodb+srv://u0:GuBCHfJV54RVyY4M@cluster0.8vjcbyf.mongodb.net/?retryWrites=true&w=majority'
+const filename = "./USDOT_80806_All_BASICs_Public_07-28-2023.xml";
+
+const data = ExtractInspections(filename);
+// console.log(
+//     data.vehicles.length,
+//     data.inspections.length,
+//     data.violations.length,
+// );
+
+// console.log(data.violations.slice(0, 2));
+
+// unit type: 1, 2, D
+// violations: array
+// basic is from violation summary
+
+/******************************************** */
+// INSERT TO DB
+const client = new MongoClient(url);
+await client.connect();
+console.log("connected to db");
+const db = client.db('dot_inspection');
+
+const insertMany = async (col_name, records, indexes) => {
+    try {
+        const col = db.collection(col_name);
+        await col.insertMany(
+            records,
+            { ordered: false }
+        );
+        await col.createIndex(indexes)
+        return records.length;
+    } catch (e) {
+        if (e.code == 11000) {
+            // ignore
+            return e.result.insertedCount;;
+        }
+        console.log(e);
+        return 0;
+    }
+}
+
+await insertMany('inspections', data.inspections, [['vins'], ['date', -1]]);
+await insertMany('vehicles', data.vehicles, [['license_number']]);
+await insertMany('violations', data.violations, [['vin']]);
+
+process.exit(0);
---
+++ upload/xml_data.mjs
@@ -0,0 +1,85 @@
+import { XMLParser } from 'fast-xml-parser';
+import { readFileSync } from 'fs';
+
+export function ExtractInspections(filename) {
+    const parser = new XMLParser({
+        ignoreAttributes: false,
+    });
+    const raw = readFileSync(filename);
+    const obj = parser.parse(raw);
+    /** @type {Array} */
+    const records = obj.carrierData.inspections.inspection || [];
+    const data = records.map(makeInspection);
+    const inspections = [];
+    const vehicleMap = new Map();
+    const violationList = [];
+    for (const d of data) {
+        const { vehicles, violations, ...ins } = d;
+        inspections.push(ins);
+        vehicles.forEach(v => vehicleMap.set(v._id, v));
+        if (violations) {
+            for (const v of violations) {
+                v.ins = ins._id;
+                const vehicle = vehicles.find(ve => ve.unit == v.unit) || vehicles[0];
+                v.vin = vehicle._id;
+                v._id = `${v.ins}_${v.vin}`;
+                violationList.push(v);
+            }
+        }
+    }
+    const vehicles = [...vehicleMap.values()];
+    return { inspections, vehicles, violations: violationList };
+}
+
+/************************************************************************************** */
+function makeVehicle(obj) {
+    const _id = obj['@_vehicle_id_number'] || obj['@_license_number'];
+    if (!_id) {
+        return null;
+    }
+    const license_state = obj['@_license_state'];
+    const license_number = obj['@_license_number'];
+    const type = obj['@_unit_type'];
+    const unit = +obj['@_unit'];
+    return { _id, license_state, license_number, type, unit };
+}
+
+function makeViolation(obj) {
+    const code = obj['@_code'];
+    if (!code) {
+        return null;
+    }
+    const oos = obj["@_oos"] == 'Yes';
+    const unit = obj['@_unit'];
+    return { code, oos, unit };
+}
+
+function makeInspection(obj) {
+    const date = new Date(obj['@_inspection_date']);
+    const state = obj['@_report_state'];
+    const _id = obj['@_report_number'];
+    const level = +obj['@_level'];
+    const weight = +obj['@_time_weight'];
+    const hm = obj['@_HM_inspection'] == 'Yes';
+    const phm = obj['@_Placarable_HM_Veh_Insp'] == 'Yes';
+    const vehicles = (obj.vehicles.vehicle || []).map(makeVehicle).filter(v => v);
+    const violations = [];
+    let violation = obj.violations.violation;
+    if (violation) {
+        if (!Array.isArray(violation)) {
+            violation = [violation];
+        }
+        for (const v of violation) {
+            const vi = makeViolation(v);
+            if (vi) {
+                violations.push(vi);
+            }
+        }
+    }
+    const vins = vehicles.map(v => v._id);
+    return {
+        _id, date, vins,
+        state, level, weight, hm, phm,
+        vehicles, violations,
+    };
+}
